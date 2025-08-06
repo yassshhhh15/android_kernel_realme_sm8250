118af11e2b1bc20b5f53c672a92ebd0c6f6bd050
LuK1337 <priv.luk@gmail.com>
2025-08-06 12:17:12 +0800

    oplus_sensor_devinfo: Add FW backwards compatibility

--- drivers/soc/oplus/sensor/oplus_sensor_devinfo.c
+++ drivers/soc/oplus/sensor/oplus_sensor_devinfo.c
@@ -526,8 +526,7 @@ static void parse_accelerometer_sensor_dts(struct sensor_hw *hw, struct device_n
 	int rc = 0;
 	int di = 0;
 	char *feature[] = {
-		"use-sois",
-		"single-acc"
+		"use-sois"
 	};
 
 	hw->feature.feature[0] = 0;/*default not use s-ois */
@@ -611,14 +610,8 @@ static void parse_lux_aod_sensor_dts(struct sensor_algorithm *algo, struct devic
 		algo->parameter[2] = value;
 	}
 
-	rc = of_property_read_u32(ch_node, "fold-feature", &value);
-
-	if (!rc) {
-		algo->feature[0] = value;
-	}
-
-	SENSOR_DEVINFO_DEBUG("thrd-low: %d, thrd-high: %d, als-type: %d, fold-feature: %d\n",
-		algo->parameter[0], algo->parameter[1], algo->parameter[2], algo->feature[0]);
+	SENSOR_DEVINFO_DEBUG("thrd-low: %d, thrd-high: %d, als-type: %d\n",
+		algo->parameter[0], algo->parameter[1], algo->parameter[2]);
 
 }
 
