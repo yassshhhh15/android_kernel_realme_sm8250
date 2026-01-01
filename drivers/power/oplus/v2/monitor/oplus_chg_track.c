cc01bf0ee9f364eb8f576bf27b25be822fdb42e0
Mashopy <eliasgheeraert@gmail.com>
2026-01-02 00:20:29 +0800

    treewide: Drop Oplus security changes
    * Oplus security changes completely nuke support SELinux permissive, block system mounting and spam logs for nothing
    
    Signed-off-by: ermyltsor <lamiimuk@163.com>

--- drivers/power/oplus/v2/monitor/oplus_chg_track.c
+++ drivers/power/oplus/v2/monitor/oplus_chg_track.c
@@ -26,8 +26,6 @@
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) ||	\
 	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 #include <soc/oplus/system/kernel_fb.h>
-#elif defined(CONFIG_OPLUS_KEVENT_UPLOAD)
-#include <linux/oplus_kevent.h>
 #endif
 #ifndef CONFIG_DISABLE_OPLUS_FUNCTION
 #include <soc/oplus/system/oplus_project.h>
@@ -576,8 +574,7 @@ struct oplus_chg_track {
 
 	struct workqueue_struct *trigger_upload_wq;
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) || \
-	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE) || \
-	defined(CONFIG_OPLUS_KEVENT_UPLOAD)
+	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 	struct kernel_packet_info *dcs_info;
 #endif
 	struct delayed_work upload_info_dwork;
@@ -677,8 +674,7 @@ static DEFINE_MUTEX(debugfs_root_mutex);
 static DEFINE_SPINLOCK(gauge_fifo_lock);
 
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) || \
-	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE) || \
-	defined(CONFIG_OPLUS_KEVENT_UPLOAD)
+	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 static int oplus_chg_track_pack_dcs_info(struct oplus_chg_track *chip);
 #endif
 static int
@@ -693,8 +689,7 @@ static bool oplus_chg_track_get_mmi_chg(void);
 static int oplus_chg_track_obtain_power_info(char *power_info, int len);
 
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) || \
-	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE) || \
-	defined(CONFIG_OPLUS_KEVENT_UPLOAD)
+	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 static struct type_reason_table track_type_reason_table[] = {
 	{ TRACK_NOTIFY_TYPE_DEFAULT, "default" },
 	{ TRACK_NOTIFY_TYPE_SOC_JUMP, "soc_error" },
@@ -3172,8 +3167,7 @@ static int oplus_chg_track_init(struct oplus_chg_track *track_dev)
 }
 
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) || \
-	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE) || \
-	defined(CONFIG_OPLUS_KEVENT_UPLOAD)
+	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 static int oplus_chg_track_get_type_tag(int type_reason, char *type_reason_tag)
 {
 	int i = 0;
@@ -3356,8 +3350,7 @@ static int oplus_chg_track_thread(void *data)
 		mutex_lock(&chip->trigger_data_lock);
 		chip->trigger_data_ok = false;
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) || \
-	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE) || \
-	defined(CONFIG_OPLUS_KEVENT_UPLOAD)
+	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 		oplus_chg_track_pack_dcs_info(chip);
 #endif
 		chip->dwork_retry_cnt = OPLUS_CHG_TRACK_DWORK_RETRY_CNT;
@@ -3440,8 +3433,7 @@ static int oplus_chg_track_get_local_time_s(void)
 #define TRACK_VERSION	"3.4"
 
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) || \
-	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE) || \
-	defined(CONFIG_OPLUS_KEVENT_UPLOAD)
+	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 static int oplus_chg_track_pack_dcs_info(struct oplus_chg_track *chip)
 {
 	int ret = 0;
@@ -3513,8 +3505,6 @@ static void oplus_chg_track_upload_info_dwork(struct work_struct *work)
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) ||	\
 	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 	ret = fb_kevent_send_to_user(chip->dcs_info);
-#elif defined(CONFIG_OPLUS_KEVENT_UPLOAD)
-	ret = kevent_send_to_user(chip->dcs_info);
 #endif
 	mutex_unlock(&chip->dcs_info_lock);
 	if (!ret)
@@ -7024,8 +7014,7 @@ int oplus_chg_track_driver_init(struct oplus_monitor *monitor)
 	track_dev->monitor = monitor;
 
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) || \
-	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE) || \
-	defined(CONFIG_OPLUS_KEVENT_UPLOAD)
+	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 	track_dev->dcs_info = (struct kernel_packet_info *)kmalloc(
 		sizeof(char) * OPLUS_CHG_TRIGGER_MSG_LEN, GFP_KERNEL);
 	if (track_dev->dcs_info == NULL) {
@@ -7097,8 +7086,7 @@ parse_dt_err:
 		debugfs_remove_recursive(track_debugfs_root);
 debugfs_create_fail:
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) || \
-	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE) || \
-	defined(CONFIG_OPLUS_KEVENT_UPLOAD)
+	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 	kfree(track_dev->dcs_info);
 dcs_info_kmalloc_fail:
 #endif
@@ -7127,8 +7115,7 @@ int oplus_chg_track_driver_exit(struct oplus_monitor *monitor)
 	if (track_debugfs_root)
 		debugfs_remove_recursive(track_debugfs_root);
 #if defined(CONFIG_OPLUS_FEATURE_FEEDBACK) || \
-	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE) || \
-	defined(CONFIG_OPLUS_KEVENT_UPLOAD)
+	defined(CONFIG_OPLUS_FEATURE_FEEDBACK_MODULE)
 	kfree(track_dev->dcs_info);
 #endif
 	devm_kfree(monitor->dev, track_dev);
